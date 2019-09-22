ca36fa3402cce0760a0983d9efa7616fc7525c8b
OMSF <ocimar.acaD@gmail.com>
2019-09-22 20:19:04 -0300

    Automata constructor finished

--- Automata/automata.cpp
+++ Automata/automata.cpp
@@ -1,5 +1,6 @@
 #include "automata.h"
 #include <stack>
+#include <forward_list>
 
 Edge::Edge(int index1, int index2, string reg_exprsn) {
     node1 = index1;
@@ -11,17 +12,16 @@ Operation::Operation(string reg_exprsn1, string reg_exprsn2, char language_opera
     reg_expr1 = reg_exprsn1;
     reg_expr2 = reg_exprsn2;
     lang_operator = language_operator;
-
 }
 
-Graph::Graph(int num_initial_nodes) {
-    size = num_initial_nodes;
+Graph::Graph() {
+    size = 0;
 }
 
 bool Graph::add_edge(int index1, int index2, string reg_expr){
     if(index1< size && index2 < size){
         Edge aux(index1, index2, reg_expr);
-        edge_list.push_back(aux);
+        edge_list.push_front(aux);
         return true;
     }
     else
@@ -39,35 +39,89 @@ int Graph::new_node(bool is_final) { //retorna o indice do nó
     return size-1;
 }
 
-Operation Automata::expr_splitter(string reg_expr) {
+Operation Automata::expr_splitter(string orig_expr) {
+    string reg_expr1, reg_expr2;
+    char lang_operator;
+    int num_open_parentheses = 0,
+        last_open_parentheses = -1; // posição do ultimo parentese aberto. -1 indica que nenhum parentese foi aberto
+    vector<int> open_parentheses;
 
+    for (int i = 0; i < orig_expr.size(); ++i) {
+        if(num_open_parentheses == 0){
+            if(orig_expr[i] == '+'){
+                reg_expr1 = string(orig_expr.begin(), orig_expr.begin()+i);
+                reg_expr2 = string(orig_expr.begin()+i+1, orig_expr.end());
+                lang_operator = '+';
+            }
+            if(orig_expr[i] == '*'){ // a operação com parenteses na verdade é desnecessário, precisamos analisar as cat antes  
+                if(orig_expr[i-1] != ')'){
+                    reg_expr1 = string(1, orig_expr[i-1]);
+                }
+                else{
+                    reg_expr1 = string(orig_expr.begin()+last_open_parentheses+1, orig_expr.begin()+i);
+                }
+                lang_operator = '*';
+            }
+        }
+
+        if(orig_expr[i]=='('){
+            num_open_parentheses++;
+            open_parentheses.push_back(i);
+            last_open_parentheses = i;
+        }
+
+    }
+    Operation answer(reg_expr1, reg_expr2, lang_operator);
 }
 
 Automata::Automata(string reg_expr) {
     // Construtor
     size = 2;
 
-    automata_graph = Graph(2);
+    automata_graph.new_node(false);//nó inicial (nó de num 0)
+    automata_graph.new_node(true);//nó final (nó de num 1)
     automata_graph.add_edge(0, 1, reg_expr);
 
-    stack<Edge> remaining_edges; // armazena as aresas que podem ser reduzidas
-    remaining_edges.push(automata_graph.edge_list[0]); // adiciona a única aresta que o grafo tem por enquanto
+    stack<list<Edge>::iterator> remaining_edges; // armazena o ponteiro das aresas que podem ser reduzidas
+    remaining_edges.push(automata_graph.edge_list.begin()); // adiciona a única aresta que o grafo tem por enquanto
 
     while (!remaining_edges.empty()){ //enquanto houver arestas que podem ser reduzidas
-        Edge local_edge = remaining_edges.top();
-        Operation aux_op = expr_splitter(local_edge.reg_expr);
+        auto local_edge_ptr = remaining_edges.top();
+        Operation aux_op = expr_splitter(local_edge_ptr->reg_expr);
 
         if (aux_op.lang_operator == '+'){ // União
-            modifica_aresta_correspondente // vou ter que usar lista ligada
+            local_edge_ptr->reg_expr = aux_op.reg_expr1; //atualiza a expr reg da aresta já existente para uma das da união
 
-
-        }
-        else if (aux_op.lang_operator == '*'){// feixo de Kleene
-
+            // e adiciona uma nova aresta com a outra expr reg:
+            automata_graph.add_edge(local_edge_ptr->node1, local_edge_ptr->node2, aux_op.reg_expr2);
+            remaining_edges.push(automata_graph.edge_list.begin());
         }
         else if (aux_op.lang_operator == 'c') {// concatenação
+            int local_new_node = automata_graph.new_node(false); // cria um novo nó (que ficará entre os dois analisados)
 
+            // add as arestas:
+            automata_graph.add_edge(local_edge_ptr->node1, local_new_node, aux_op.reg_expr1);
+            automata_graph.add_edge(local_new_node, local_edge_ptr->node2, aux_op.reg_expr2);
+
+            // e deleta a anterior:
+            automata_graph.edge_list.erase(local_edge_ptr);
+            remaining_edges.pop();
         }
+        else if (aux_op.lang_operator == '*'){// feixo de Kleene
+            int local_new_node = automata_graph.new_node(false); // cria um novo nó (que ficará entre os dois analisados)
+
+            // add as arestas:
+            automata_graph.add_edge(local_edge_ptr->node1, local_new_node, "&");
+            automata_graph.add_edge(local_new_node, local_edge_ptr->node2, "&");
+            automata_graph.add_edge(local_new_node, local_new_node, aux_op.reg_expr1);
+
+            // e deleta a anterior:
+            automata_graph.edge_list.erase(local_edge_ptr);
+            remaining_edges.pop();
+        }
+        else
+            remaining_edges.pop(); // não há nenhuma operação na expressão da aresta, logo não precisa mais ser reduzida
+
 
     }
 }
--- Automata/automata.h
+++ Automata/automata.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <list>
 
 using namespace std;
 
@@ -26,9 +27,9 @@ class Graph{ //implementado por lista de arco
 private:
     vector<bool> final_node;
 public:
-    vector<Edge> edge_list;
+    list<Edge> edge_list;
     int size;// numero de nó's
-    Graph(int num_initial_nodes);
+    Graph();
     bool add_edge(int index1, int index2, string reg_expr); // retorna se conseguiu incerir a aresta (não consegue
                                                             // incerir se um dos indices não se refere a um existente)
     bool is_final_node(int node_index);
@@ -38,12 +39,11 @@ public:
 class Automata{
 private:
     int size;
-    Graph automata_graph = Graph(0);
+    Graph automata_graph = Graph();
 public:
     Automata(string reg_expr);
     Operation expr_splitter(string reg_expr); // retorna uma ou duas expressoes regulares e
                                               // a operação que ocorre com/entre elas
-    bool all_edge_ok;
 
 };
 
