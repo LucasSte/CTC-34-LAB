57fa9cfc60027bf7a25dadef3789f845f4e06802
OMSF <ocimar.acaD@gmail.com>
2019-09-23 14:04:01 -0300

    Exercise1 fixed: adding a parenthesis remover

--- AutomataOcimar/automata.cpp
+++ AutomataOcimar/automata.cpp
@@ -1,6 +1,7 @@
 #include "automata.h"
 #include <stack>
 #include <iostream>
+#include <sstream>
 
 Edge::Edge(int index1, int index2, string reg_exprsn) {
     node1 = index1;
@@ -44,7 +45,8 @@ Operation Automata::expr_splitter(string orig_expr) {
     char lang_operator;
     int num_open_parentheses = 0;
     bool there_is_union = false,
-            there_is_cat = false;
+         there_is_cat = false,
+         first_parentheses_closed = false;
 
     for (int i = 0; i < orig_expr.size() && !there_is_union; ++i) {
         if(num_open_parentheses == 0 && i!=0){
@@ -54,32 +56,23 @@ Operation Automata::expr_splitter(string orig_expr) {
                 lang_operator = '+';
                 there_is_union = true;
             }
-            else if(isalpha(orig_expr[i])){ // Concatenação com um char
-                reg_expr1 = string(orig_expr.begin(), orig_expr.begin()+i);
-                reg_expr2 = string(orig_expr.begin()+i, orig_expr.end());
-                lang_operator = 'c';
-                there_is_cat = true;
-            }
-            else if(orig_expr[i] == '('){ // Concatenação com uma expressão entre parenteses
-                reg_expr1 = string(orig_expr.begin(), orig_expr.begin()+i);
-                reg_expr2 = string(orig_expr.begin()+i+1, orig_expr.end()-1);
+            else if(isalpha(orig_expr[i]) || orig_expr[i] == '(') {
+                reg_expr1 = string(orig_expr.begin(), orig_expr.begin() + i);
+                reg_expr2 = string(orig_expr.begin() + i, orig_expr.end());
                 lang_operator = 'c';
                 there_is_cat = true;
             }
             else if(orig_expr[i] == '*' && !there_is_cat){  // Feixo de Kleene
+                lang_operator = '*';
                 if (isalpha(orig_expr[i-1])){ // a expressão é um único char
                     reg_expr1 = string(orig_expr.begin(), orig_expr.end()-1);
-                    lang_operator = '*';
                 }
                 else{// a expressão é limitado por parenteses
                     reg_expr1 = string(orig_expr.begin()+1, orig_expr.end()-2); // não considera os parenreses inicial e final
-                    lang_operator = '*';
                 }
 
             }
-//            else {//error
-//                lang_operator = 'x';
-//            }
+
         }
 
         if(orig_expr[i]=='('){
@@ -87,6 +80,13 @@ Operation Automata::expr_splitter(string orig_expr) {
         }
         else if(orig_expr[i]==')'){
             num_open_parentheses--;
+            if(num_open_parentheses == 0 && !first_parentheses_closed){ // se o primeiro parenteses é fechado só agora
+                first_parentheses_closed = true;
+                if(i == orig_expr.size()-1){// e agora é o final da expressão, então ele pode ser removido
+                    reg_expr1 = string(orig_expr.begin()+1, orig_expr.end()-1);
+                    lang_operator = 'p'; // remoção de parenteses
+                }
+            }
         }
 
     }
@@ -147,6 +147,9 @@ Automata::Automata(string reg_expr) {
             // e deleta a anterior do grafo
             automata_graph.edge_list.erase(local_edge_ptr);
         }
+        else if(aux_op.lang_operator == 'p'){ // remoção de parenteses
+            local_edge_ptr->reg_expr = aux_op.reg_expr1; //atualiza a expr reg da aresta já existente, removendo os parenteses
+        }
         else
             remaining_edges.pop(); // não há nenhuma operação na expressão da aresta, logo não precisa mais ser reduzida
 
@@ -165,4 +168,44 @@ void Automata::show_graph() {
         }
     }
     cout<<"}"<<endl;
+}
+vector<int> Automata::getNextSates(int node, char letter){ //valores negativos indica que o estado é alcançado por meio de uma transição &
+    vector<int> next_states;
+    for(auto edge : automata_graph.edge_list){
+        if(edge.node1 == node){
+            if(edge.reg_expr[0] == letter){
+                next_states.push_back(edge.node2);
+            }
+            if(edge.reg_expr[0] == '&'){
+                next_states.push_back(-edge.node2);
+            }
+        }
+    }
+    return next_states;
+}
+string Automata::states_after_computation(int start_node, string str) {
+    ostringstream output;
+
+    if (str.size() == 0){ // o estado atual é o estado de fim da computação
+        if(automata_graph.is_final_node(start_node)){
+            output<<start_node<<" - Final state (String accepted)"<<endl;
+        }
+        else{
+            output<<start_node<<endl;
+        }
+    }
+
+    vector<int> next_states = getNextSates(start_node, str[0]);
+    for(auto next_node : next_states){
+        string aux;
+        if(next_node > 0){
+            aux = string(str.begin()+1, str.end());
+        }
+        else{ // o nó é alcançado por meio de uma transição epsilon
+            aux = str;
+        }
+        output<<states_after_computation(abs(next_node), aux);
+    }
+    return output.str();
+
 }
--- AutomataOcimar/automata.h
+++ AutomataOcimar/automata.h
@@ -23,7 +23,7 @@ public:
     char lang_operator;
 };
 
-class Graph{ //implementado por lista de arco
+class Graph{ //implementado por lista de arcos
 private:
     vector<bool> final_node;
 public:
@@ -32,6 +32,7 @@ public:
     Graph();
     bool add_edge(int index1, int index2, string reg_expr); // retorna se conseguiu incerir a aresta (não consegue
     // incerir se um dos indices não se refere a um existente)
+
     bool is_final_node(int node_index);
     int new_node(bool is_final); //retorna o indice do nó
 };
@@ -43,7 +44,9 @@ public:
     Automata(string reg_expr);
     Operation expr_splitter(string reg_expr); // retorna uma ou duas expressoes regulares e
     // a operação que ocorre com/entre elas
-    void show_graph();
+    void show_graph(); // printa o grafo na linguagem dot
+    string states_after_computation(int start_node, string str);
+    vector<int> getNextSates(int node, char letter);
 
 };
 
